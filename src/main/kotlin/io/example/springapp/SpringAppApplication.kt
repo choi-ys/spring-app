63ce021f57a604062d50b9b3cace54111fa01971
choi-ys <project.log.062@gmail.com>
2022-02-25 01:41:01 +0900

    Initial commit

---
+++ src/main/kotlin/io/example/springapp/SpringAppApplication.kt
@@ -0,0 +1,11 @@
+package io.example.springapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAppApplication>(*args)
+}
